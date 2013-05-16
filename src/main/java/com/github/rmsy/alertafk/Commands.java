702ed0800000f0dfa6c6d4b22285d48c4712c45b
Isaac Moore <rmsy@me.com>
2013-05-16 10:49:25 -0500

    Finish afkCommand() and setAfkCommand()

--- src/main/java/com/github/rmsy/alertafk/Commands.java
+++ src/main/java/com/github/rmsy/alertafk/Commands.java
@@ -10,9 +10,9 @@
 
 package com.github.rmsy.alertafk;
 
-import com.sk89q.minecraft.util.commands.Command;
-import com.sk89q.minecraft.util.commands.CommandContext;
-import com.sk89q.minecraft.util.commands.CommandException;
+import com.google.common.base.Preconditions;
+import com.sk89q.minecraft.util.commands.*;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
 import javax.annotation.Nonnull;
@@ -26,6 +26,7 @@ public final class Commands {
      *
      * @param context The context in which the command was executed.
      * @param sender  The executor of the command.
+     * @throws CommandException
      */
     @Command(
             aliases = {"afk", "away"},
@@ -34,22 +35,64 @@ public final class Commands {
             flags = "p:",
             help = "This command enables AFK mode (if it is disabled) or disables AFK mode (if it is enabled).",
             min = 0,
-            usage = "<message> [-p <player>]"
+            usage = "[-p:<player>] [message]"
     )
     public void afkCommand(@Nonnull final CommandContext context, @Nonnull final CommandSender sender) throws CommandException {
-        //  Assuming that CommandManager only sends us commands from Players...
-        if (context.hasFlag('p')) {
-            t
-        }
-        org.bukkit.entity.Player target = (org.bukkit.entity.Player) sender;
-        Player aaTarget = AlertAFK.plugin.getPlayer(target.getName());
-
-        if (context.argsLength() > 0) {
+        if (Preconditions.checkNotNull(sender, "afkCommand() got null CommandSender") instanceof org.bukkit.entity.Player) {
+            org.bukkit.entity.Player target = (org.bukkit.entity.Player) sender;
+            Player aaTarget = Preconditions.checkNotNull(AlertAFK.plugin.getPlayer(target.getName()), "afkCommand() found null AAPlayer: %s", target.getDisplayName());
             if (aaTarget.isAfk()) {
                 aaTarget.setAfk(false);
+            } else {
+                if (context.argsLength() > 0) {
+                    aaTarget.setAfkWithMessage(context.getJoinedStrings(0));
+                } else {
+                    aaTarget.setAfk(true);
+                }
             }
         } else {
-            aaTarget.setAfk(!aaTarget.isAfk());
+            sender.sendMessage(ChatColor.RED + "You must be a player to execute this command!");
+        }
+    }
+
+    /**
+     * Command to toggle AFK mode for the specified player.
+     *
+     * @param context The context in which the command was executed.
+     * @param sender  The executor of the command.
+     * @throws CommandException
+     */
+    @Command(aliases = {"setafk, safk"},
+            desc = "Toggles AFK mode for the specified player.",
+            anyFlags = false,
+            help = "This command enables AFK mode (if it is disabled) or disables AFK mode (if it is enabled) for the specified player.",
+            min = 1,
+            usage = "<player> [message]"
+    )
+    @Console
+    public void setOtherAfkCommand(@Nonnull final CommandContext context, @Nonnull final CommandSender sender) throws CommandException {
+        Preconditions.checkNotNull(sender, "setOtherAfkCommand() got null CommandSender");
+
+        //  Find the target
+        org.bukkit.entity.Player target = null;
+        try {
+            target = Preconditions.checkNotNull(Bukkit.getPlayer(Preconditions.checkNotNull(context, "setOtherAfkCommand() got null CommandContext").getString(0)));
+        } catch (NullPointerException exception) {
+            sender.sendMessage(ChatColor.RED + "Player not found!");
+        }
+
+        //  Act on the target
+        Player aaTarget = Preconditions.checkNotNull(AlertAFK.plugin.getPlayer(target.getName()), "setOtherAfkCommand() found null AAPlayer: %s", target.getDisplayName());
+        if (aaTarget.isAfk()) {
+            aaTarget.setAfk(false);
+            sender.sendMessage(ChatColor.YELLOW + "You set " + target.getDisplayName() + ChatColor.RESET + ChatColor.YELLOW + " as no longer AFK.");
+        } else {
+            if (context.argsLength() > 0) {
+                aaTarget.setAfkWithMessage(context.getJoinedStrings(1));
+            } else {
+                aaTarget.setAfk(true);
+            }
+            sender.sendMessage(ChatColor.YELLOW + "You set " + target.getDisplayName() + ChatColor.RESET + ChatColor.YELLOW + " as AFK.");
         }
     }
 }
